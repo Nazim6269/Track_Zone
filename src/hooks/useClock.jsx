fe48d6a1658fddeb4f933cc4249b02a79f727657
Nazim Uddin <107185368+Nazim6269@users.noreply.github.com>
2024-03-23 02:04:16 +0600

    added different time zone

---
+++ src/hooks/useClock.jsx
@@ -0,0 +1,58 @@
+import { addMinutes } from "date-fns";
+import { useEffect, useState } from "react";
+
+const initialState = {
+  id: "",
+  title: "",
+  timezone: {
+    type: "",
+    offset: "",
+  },
+  date_utc: null,
+  date: null,
+};
+
+const TIMEZONE_OFFSET = {
+  PST: -7 * 60,
+  EST: -4 * 60,
+  EDT: -4 * 60,
+  BST: 1 * 60,
+  MST: -6 * 60,
+};
+
+const useClock = (timezone, offset = 0) => {
+  const [state, setState] = useState({ ...initialState });
+  const [utc, setUTC] = useState(null);
+
+  useEffect(() => {
+    let d = new Date();
+    const localOffset = d.getTimezoneOffset();
+    d = addMinutes(d, localOffset);
+    setUTC(d);
+  }, []);
+
+  useEffect(() => {
+    if (utc !== null && timezone) {
+      offset = TIMEZONE_OFFSET[timezone] ?? offset;
+
+      const newUTC = addMinutes(utc, offset);
+      setState({
+        ...state,
+        date_utc: utc,
+        date: newUTC,
+      });
+    } else {
+      setState({
+        ...state,
+        date_utc: utc,
+        date: utc,
+      });
+    }
+  }, [utc]);
+
+  return {
+    clock: state,
+  };
+};
+
+export default useClock;
