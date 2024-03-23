146e71a4f9d4ebb48df361b4ed279932b42b1e90
Nazim Uddin <107185368+Nazim6269@users.noreply.github.com>
2024-03-23 20:00:31 +0600

    added some refactoring in useClock hook

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,8 @@ import "./App.css";
 import useClock from "./hooks/useClock";
 
 function App() {
-  const { clock: local } = useClock("Local");
-  const { clock: pst } = useClock("PST");
-  const { clock: est } = useClock("EST");
-  const { clock: edt } = useClock("EDT");
-  const { clock: bst } = useClock("BST");
-  const { clock: mst } = useClock("MST");
-  const { clock: pakistan } = useClock("UTC", 5 * 60);
-
-  console.log("Local", local.date);
-  console.log("PST", pst.date);
-  console.log("EST", est.date);
-  console.log("EDT", edt.date);
-  console.log("BST", bst.date);
-  console.log("MST", mst.date);
-  console.log("Pakistan", pakistan.date);
+  const { date: localDate } = useClock();
+  console.log(localDate);
 
   return <h1>Track Zone</h1>;
 }
--- src/hooks/useClock.jsx
+++ src/hooks/useClock.jsx
@@ -1,17 +1,6 @@
 import { addMinutes } from "date-fns";
 import { useEffect, useState } from "react";
 
-const initialState = {
-  id: "",
-  title: "",
-  timezone: {
-    type: "",
-    offset: "",
-  },
-  date_utc: null,
-  date: null,
-};
-
 const TIMEZONE_OFFSET = {
   PST: -7 * 60,
   EST: -4 * 60,
@@ -21,37 +10,38 @@ const TIMEZONE_OFFSET = {
 };
 
 const useClock = (timezone, offset = 0) => {
-  const [state, setState] = useState({ ...initialState });
+  const [localDate, setLocalDate] = useState(null);
+  const [localOffset, setLocalOffset] = useState(0);
   const [utc, setUTC] = useState(null);
 
   useEffect(() => {
     let d = new Date();
-    const localOffset = d.getTimezoneOffset();
-    d = addMinutes(d, localOffset);
+    const lo = d.getTimezoneOffset();
+    d = addMinutes(d, lo);
     setUTC(d);
+    setLocalOffset(lo);
   }, []);
 
   useEffect(() => {
-    if (utc !== null && timezone) {
-      offset = TIMEZONE_OFFSET[timezone] ?? offset;
+    if (utc !== null) {
+      if (timezone) {
+        offset = TIMEZONE_OFFSET[timezone] ?? offset;
 
-      const newUTC = addMinutes(utc, offset);
-      setState({
-        ...state,
-        date_utc: utc,
-        date: newUTC,
-      });
-    } else {
-      setState({
-        ...state,
-        date_utc: utc,
-        date: utc,
-      });
+        const newUTC = addMinutes(utc, offset);
+        setLocalDate(newUTC);
+      } else {
+        const newUTC = addMinutes(utc, localOffset);
+        setLocalDate(newUTC);
+      }
     }
   }, [utc]);
 
   return {
-    clock: state,
+    date: localDate,
+    dateUTC: utc,
+    offset,
+    timezone,
+    localOffset,
   };
 };
 
